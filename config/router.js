bc5b937da44d86b79775a6c09774fc7e791d666d
atzalameda <atzalameda@gmail.com>
2015-08-27 15:31:36 +0800

    Connect MySQL with NodeJS

--- config/router.js
+++ config/router.js
@@ -3,18 +3,18 @@ var teacher = require('./../controllers/teacher');
 
 module.exports = function(router){//exportable functions. To export modules to other modules
 	router.route('/students')
-	.get(student.find);//execute the function in student.js
+	.get(student.find)//execute the function in student.js
+	.post(student.insert);
+	
+	router.route('/students/:id')
+	.get(student.findOne)
+	.put(student.update)
+	.delete(student.remove);
 	
 	router.route('/teachers')
-	.get(teacher.view);
-	
-	router.route('/teachers')
-	.put(teacher.add);
-	
-	router.route('/teachers')
-	.post(teacher.update);
-	
-	router.route('/teachers')
+	.get(teacher.view)
+	.put(teacher.add)
+	.post(teacher.update)
 	.delete(teacher.del);
 	
 	return router;
